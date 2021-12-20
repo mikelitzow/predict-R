2c5cf682d92895eb2ca42c3704e7e3f54900fe31
mikelitzow <mike.litzow@noaa.gov>
2021-12-20 08:47:29 -0900

    Adding script to count seines in long-term and large spatial scale seine surveys

---
+++ scripts/seine_sample_size.R
@@ -0,0 +1,17 @@
+# check beach seine sample size for ms.
+
+library(tidyverse)
+
+dat <- read.csv("./data/cpue.data.csv")
+
+head(dat)
+unique(dat$bay)
+nrow(dat)
+# separate into two groups - long-term sites and broader survey
+g1 <- nrow(filter(dat, bay %in% c("Cook Bay", "Anton Larson Bay")))
+g1
+
+g2 <- nrow(filter(dat, !bay %in% c("Cook Bay", "Anton Larson Bay")))
+g2
+
+g1+g2
